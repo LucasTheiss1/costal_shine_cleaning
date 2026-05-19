30a348db649d3f3614ffc1c6c3b5e31838bd4c35
LucasTheiss1 <lucasptheiss@gmail.com>
2026-05-19 16:04:54 +0100

    Feat: smooth scrolling for navbar links in mobile screens

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+// Handle form submission
 const form = document.getElementById("form");
 const status = document.getElementById("form-status");
 
@@ -20,4 +21,18 @@ form.addEventListener("submit", async function (event) {
   } else {
     status.textContent = "Something went wrong. Please try again.";
   }
+});
+
+// Smooth scrolling for navigation links
+const navLinks = document.querySelectorAll('.navbar-collapse .nav-link');
+const navbarCollapse = document.querySelector('.navbar-collapse');
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    const bsCollapse = bootstrap.Collapse.getInstance(navbarCollapse);
+
+    if (bsCollapse) {
+      bsCollapse.hide();
+    }
+  });
 });
